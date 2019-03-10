2503ee775c720e7e02277b69f4740d98ddee51f1
TheWifflebrain <doe15002@byui.edu>
2019-03-09 18:57:10 -0700

    made the gps and settings button work like we want it to, and made the text on the settings activity look good. commented out the back button because it reset the map activity. need to learn how to keep the info from previous activity or just use the back button on the phone itself

--- app/src/main/java/com/example/almostthere/SettingsActivity.java
+++ app/src/main/java/com/example/almostthere/SettingsActivity.java
@@ -1,13 +1,33 @@
 package com.example.almostthere;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Set;
 
 public class SettingsActivity extends AppCompatActivity {
+    private ImageView buttonBack;
+    private static final String TAG = "SettingsActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings2);
+        //buttonBack = (ImageView) findViewById(R.id.backToMain);
+
+//        buttonBack.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Log.d(TAG, "onClick: clicked settings icon");
+//                Intent intent = new Intent(SettingsActivity.this, MapActivity.class);
+//                startActivity(intent);
+//            }
+//        });
     }
+
 }
