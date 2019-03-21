5ad18eebbaa37cb9e00bf97ae3525a9c9c984471
TheWifflebrain <doe15002@byui.edu>
2019-03-21 00:56:47 -0600

    The clculationbydistance function had lines to convert it into km and meters for the log and now that is gone, it only uses miles now

--- app/src/main/java/com/example/almostthere/MapActivity.java
+++ app/src/main/java/com/example/almostthere/MapActivity.java
@@ -259,15 +259,10 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                 Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
                         Math.sin(dLon/2) * Math.sin(dLon/2);
         double c = 2 * Math.asin(Math.sqrt(a));
-        double valueResult= Radius*c;
-        double km=valueResult/1;
-        DecimalFormat newFormat = new DecimalFormat("####");
-        int kmInDec =  Integer.valueOf(newFormat.format(km));
-        double meter=valueResult%1000;
-        int  meterInDec= Integer.valueOf(newFormat.format(meter));
-        Log.i("Radius Value",""+valueResult+"   KM  "+kmInDec+" Meter   "+meterInDec);
+        double valueResult= (Radius*c)/1.609;
+        Log.i("Radius Value",""+valueResult+"  in miles.");
 
-        return ((Radius * c)/1.609);
+        return valueResult;
     }
 
     private void getDeviceLocation() {
