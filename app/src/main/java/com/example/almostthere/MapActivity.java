2aac08bfe8a8b266d38eb79883aab57648502412
TheWifflebrain <doe15002@byui.edu>
2019-03-25 23:18:27 -0600

    changed variable names to make more sense, deleted unused imports

--- app/src/main/java/com/example/almostthere/MapActivity.java
+++ app/src/main/java/com/example/almostthere/MapActivity.java
@@ -6,10 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Geocoder;
 import android.location.Location;
-import android.media.Image;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
@@ -18,12 +15,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.WindowManager;
-import android.view.inputmethod.EditorInfo;
-import android.widget.AdapterView;
-import android.widget.AutoCompleteTextView;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -31,149 +23,73 @@ import android.widget.Toast;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.PendingResult;
-import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.places.AutocompletePrediction;
-import com.google.android.gms.location.places.Place;
-import com.google.android.gms.location.places.PlaceBuffer;
-import com.google.android.gms.location.places.Places;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
-//import com.google.maps.android.SphericalUtil;
-
-import java.io.IOException;
-import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.List;
-
-import static com.google.android.libraries.places.internal.lf.r;
-import static java.lang.Math.abs;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback,
         GoogleApiClient.OnConnectionFailedListener{
+
+    //var for logs
     private static final String TAG = "MapActivity";
 
+    //vars for google maps and its permissions
+    private GoogleMap mMap;
     private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
     private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
-    private static final float DEFAULT_ZOOM = 15f;
-    private static final LatLngBounds LAT_LNG_BOUNDS = new LatLngBounds(
-            new LatLng(-40, -168), new LatLng(71, 136));
-
-
-    //widgets
-    private AutoCompleteTextView mSearchText;
-    private ImageView mGps;
-    private ImageView dGps;
-    private ImageView breakPin;
-
-    //vars
     private Boolean mLocationPermissionsGranted = false;
-    private GoogleMap mMap;
     private FusedLocationProviderClient mFusedLocationProviderClient;
-    //private PlaceAutocompleteAdapter mPlaceAutocompleteAdapter;
-    private GoogleApiClient mGoogleApiClient;
-    //private PlaceInfo mPlace;
     private static final int ERROR_DIALOG_REQUEST = 9001;
-    private ImageView buttonSettings;
 
-    //Location location;
-    private ImageView setPin;
-    private double longitude;
-    private double latitude;
-
-    private double startLongitude;
-    private double startLatitude;
-
-    private double endLongitude;
-    private double endLatitude;
-    private Handler handler = new Handler();
-    double newDistance = 0.0;
+    //camera vars
+    private static final float DEFAULT_ZOOM = 15f;
     Boolean setCamera = true;
     Boolean setMoveToCurrentLocation = true;
 
-    SharedPreferences shared;
+    //widgets
+    private ImageView startPinGps;
+    private ImageView endPinGps;
+    private ImageView breakPin;
+    private ImageView buttonSettings;
+    private ImageView setPin;
+
+    //locations vars
+    private double startLongitude;
+    private double startLatitude;
+    private double endLongitude;
+    private double endLatitude;
+    double newDistance = 0.0;
+    MarkerOptions options = null;
+
+    //radius vars
     public String radiusS = "0.25";
     public Double radiusD = 0.25;
 
+    //repeating function var
+    private Handler handler = new Handler();
+
+    //shared prefs vars
     public static final String RADIUS_SETTINGS = "RADIUS_SETTINGS";
     public static final String APP_PREFS = "APPLICATION_PREFERENCES";
-
-    //public Boolean moveCameraWithYou = false;
-
-
-    MarkerOptions options = null;
-
-    public MapActivity() {
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
-    }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        //Toast.makeText(this, "Map is Ready", Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "onMapReady: map is ready");
-        mMap = googleMap;
-
-        if (mLocationPermissionsGranted) {
-            getDeviceLocation(setCamera = true);
-
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                    != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
-                    Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                return;
-            }
-            mMap.setMyLocationEnabled(true);
-            mMap.getUiSettings().setMyLocationButtonEnabled(false);
-
-            init();
-        }
-    }
-
-    public boolean isServicesOK() {
-        Log.d(TAG, "isServicesOK: checking google services version");
-
-        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MapActivity.this);
-
-        if (available == ConnectionResult.SUCCESS) {
-            //everything is fine and the user can make map requests
-            Log.d(TAG, "isServicesOK: Google Play Services is working");
-            return true;
-        } else if (GoogleApiAvailability.getInstance().isUserResolvableError(available)) {
-            //an error occured but we can resolve it
-            Log.d(TAG, "isServicesOK: an error occured but we can fix it");
-            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MapActivity.this, available, ERROR_DIALOG_REQUEST);
-            dialog.show();
-        } else {
-            Toast.makeText(this, "You can't make map requests", Toast.LENGTH_SHORT).show();
-        }
-        return false;
-    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
-        //mSearchText = (AutoCompleteTextView) findViewById(R.id.input_search);
-        mGps = (ImageView) findViewById(R.id.ic_gsp);
+
+        startPinGps = (ImageView) findViewById(R.id.ic_gsp);
         buttonSettings = (ImageView) findViewById(R.id.settingsIV);
         setPin = (ImageView) findViewById(R.id.ic_set);
         breakPin = (ImageView) findViewById(R.id.ic_break);
-        dGps = (ImageView) findViewById(R.id.ic_locateFinalDestination);
+        endPinGps = (ImageView) findViewById(R.id.ic_locateFinalDestination);
 
         if (isServicesOK()) {
             getLocationPermission();
@@ -188,8 +104,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     private void init() {
         Log.d(TAG, "init: initializing");
 
-
-        mGps.setOnClickListener(new View.OnClickListener() {
+        startPinGps.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 setMoveToCurrentLocation = true;
@@ -199,7 +114,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
             }
         });
 
-        dGps.setOnClickListener(new View.OnClickListener() {
+        endPinGps.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.d(TAG, "onClick: clicked dgps icon");
@@ -235,7 +150,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                 else{
                     mMap.clear();
                     options = null;
-                   options = new MarkerOptions()
+                    options = new MarkerOptions()
                             .position(view);
                     mMap.addMarker(options);
                 }
@@ -258,10 +173,9 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                         handler.postDelayed(runnable, 3000);
                     }
                     else{
-                        Log.d(TAG, "Stopped repeating function 2");
+                        Log.d(TAG, "Stopped repeating updating distance in setPin");
                     }
                 }
-
             }
         });
 
@@ -304,7 +218,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                     handler.postDelayed(this, 3000);
                 }
                 else{
-                    Log.d(TAG, "not repeating function");
+                    Log.d(TAG, "stopped updating the distance in runnable");
                 }
             }
         }
@@ -339,26 +253,24 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
             radiusD = Double.parseDouble(radiusS);
         }
         Log.d(TAG, "radiusMapAct = " + radiusD);
-        return radiusD;
 
+        return radiusD;
     }
 
     public double CalculationByDistance(double startLat, double startLong, double endLat, double endLong) {
-        int Radius=6371;//radius of earth in Km
-        double lat1 = startLat;
-        double lat2 = endLat;
-        double lon1 = startLong;
-        double lon2 = endLong;
-        double dLat = Math.toRadians(lat2-lat1);
-        double dLon = Math.toRadians(lon2-lon1);
+        //radius of earth in Km
+        int Radius=6371;
+        double dLat = Math.toRadians(endLat-startLat);
+        double dLon = Math.toRadians(endLong-startLong);
         double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-                Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
+                Math.cos(Math.toRadians(startLat)) * Math.cos(Math.toRadians(endLat)) *
                         Math.sin(dLon/2) * Math.sin(dLon/2);
         double c = 2 * Math.asin(Math.sqrt(a));
-        double valueResult= (Radius*c)/1.609;
-        Log.i("Radius Value",""+valueResult+"  in miles.");
+        //dividing by 1.609 to go from km to miles
+        double distMiles = (Radius*c)/1.609;
+        Log.i("Radius Value",+ distMiles + "  in miles.");
 
-        return valueResult;
+        return distMiles;
     }
 
 
@@ -387,7 +299,6 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                                         DEFAULT_ZOOM,
                                         "My Location");
                             }
-
                         } else {
                             Log.d(TAG, "onComplete: current location is null");
                             Toast.makeText(MapActivity.this, "unable to get current location", Toast.LENGTH_SHORT).show();
@@ -403,15 +314,57 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     private void moveCamera(LatLng latLng, float zoom, String title) {
         Log.d(TAG, "moveCamera: moving the camera to: lat: " + latLng.latitude + ", lng: " + latLng.longitude);
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
-
     }
 
-        private void initMap() {
+    private void initMap() {
         Log.d(TAG, "initMap: initializing map");
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-
         mapFragment.getMapAsync(MapActivity.this);
+    }
 
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d(TAG, "onConnectionFailed: map failed");
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Log.d(TAG, "onMapReady: map is ready");
+        mMap = googleMap;
+
+        if (mLocationPermissionsGranted) {
+            getDeviceLocation(setCamera = true);
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            mMap.setMyLocationEnabled(true);
+            mMap.getUiSettings().setMyLocationButtonEnabled(false);
+
+            init();
+        }
+    }
+
+    public boolean isServicesOK() {
+        Log.d(TAG, "isServicesOK: checking google services version");
+
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MapActivity.this);
+
+        if (available == ConnectionResult.SUCCESS) {
+            //everything is fine and the user can make map requests
+            Log.d(TAG, "isServicesOK: Google Play Services is working");
+            return true;
+        } else if (GoogleApiAvailability.getInstance().isUserResolvableError(available)) {
+            //an error occurred but we can resolve it
+            Log.d(TAG, "isServicesOK: an error occurred but we can fix it");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MapActivity.this, available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        } else {
+            Toast.makeText(this, "You can't make map requests", Toast.LENGTH_SHORT).show();
+        }
+        return false;
     }
 
     private void getLocationPermission() {
@@ -454,16 +407,10 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                     }
                     Log.d(TAG, "onRequestPermissionsResult: permission granted");
                     mLocationPermissionsGranted = true;
-                    //initialize our map
                     initMap();
-
                 }
             }
         }
     }
-
-
-
-
 }
 
