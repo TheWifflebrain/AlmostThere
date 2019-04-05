d9cdfb69aeca4db6b24e8ddf873132c61c81f053
Steven <mitchs.patterson@gmail.com>
2019-04-05 16:33:18 -0600

    Fixed Alarm Again

--- app/src/main/java/com/example/almostthere/Alarm.java
+++ app/src/main/java/com/example/almostthere/Alarm.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.widget.Toast;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.media.Ringtone;
 
 public class Alarm extends BroadcastReceiver {
     @Override
@@ -14,12 +15,16 @@ public class Alarm extends BroadcastReceiver {
 
         Uri alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
 
-        if(alert == null){
+
+        if (alert == null) {
             alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
 
-            if(alert == null)
+            if (alert == null)
                 alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
-            }
         }
-    }
 
+        Ringtone ringtone = RingtoneManager.getRingtone(context, alert);
+        ringtone.play();
+
+    }
+}
