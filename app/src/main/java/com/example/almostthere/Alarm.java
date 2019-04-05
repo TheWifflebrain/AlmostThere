500d1d94fd7648fe0597f3be630a0dee27b148f8
Steven <mitchs.patterson@gmail.com>
2019-04-05 16:12:16 -0600

    Fixed Alarm Class

--- app/src/main/java/com/example/almostthere/Alarm.java
+++ app/src/main/java/com/example/almostthere/Alarm.java
@@ -1,4 +1,25 @@
 package com.example.almostthere;
 
-public class Alarm {
-}
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+import android.media.RingtoneManager;
+import android.net.Uri;
+
+public class Alarm extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Wake UP!!!!!", Toast.LENGTH_LONG).show();
+
+        Uri alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+
+        if(alert == null){
+            alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+            if(alert == null)
+                alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
+            }
+        }
+    }
+
